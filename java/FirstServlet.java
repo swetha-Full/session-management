025f99b9bcc540790aa84ceb0dd29e002962e4e5
swetha-Full <36259984+swetha-Full@users.noreply.github.com>
2018-03-16 13:24:35 +0530

    HttpSession

---
+++ java/FirstServlet.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "first", urlPatterns = { "/first" })
+public class FirstServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("hello first servlet");
+		String name = request.getParameter("t1");
+		System.out.println(name);
+
+		
+		HttpSession session=request.getSession();
+		session.setAttribute("t1", name);
+		 
+		// RequestDispatcher rd=request.getRequestDispatcher("/second");
+		// rd.forward(request, response);
+
+		response.sendRedirect("/second");
+	}
+}
---
+++ java/SecondServlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet(name="second",urlPatterns= {"/second"})
+
+public class SecondServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException { 
+		//response.setContentType("text/html");  
+	   Cookie cookies[]=request.getCookies();
+		PrintWriter out = response.getWriter();  
+	    
+		out.println("Second Servlet");
+	    
+		
+		HttpSession session=request.getSession();
+		String n=session.getAttribute("t1").toString();  
+	    out.print("Welcome "+n); 
+	} 
+}
